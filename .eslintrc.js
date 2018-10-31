a0a1c048612fcfb26666c12c1cabff1e95fffd89
empitsu <yoshie8080@gmail.com>
2018-10-31 13:28:43 +0900

    add rules in eslitrc for Vue.js styleguide

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,19 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'never',
+          normal: 'always',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ]
   },
   parserOptions: {
     parser: 'babel-eslint'
